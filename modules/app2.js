b80c3747da3b3c50b208a38144648f43134fd2f6
Alfredo Paz Zamora <apaz0@ucol.mx>
2020-11-18 21:56:50 -0600

    Nuevo repo

---
+++ modules/app2.js
@@ -0,0 +1,36 @@
+let bd = JSON.parse(localStorage.getItem("miBD"));
+
+if(!bd || bd == undefined){
+
+    bd = { datos: [] }
+}
+
+
+//visualizar numero de telefono de usuario en <select> APARTADO DE DEUDA
+const visualizar = () => {
+
+    let arreglo = bd.datos;
+    let recibirSelect = document.getElementById('deudores');
+
+    for(let i = 0; i<arreglo.length; i++){
+
+        let option = document.createElement('option');
+        option.innerHTML = arreglo[i].telefono;
+
+        recibirSelect.appendChild(option);
+
+    }
+};
+
+visualizar();
+
+
+// document.getElementById("btnAgregarDeuda").addEventListener("click", () => {
+
+//     let deuda = document.getElementById('txtAgregarDeuda').value;
+
+//     bd.datos.usuariox.balance.push(deuda);
+
+//     alert("Se cambió el balance");
+
+// });
---
+++ objects/usuarios.js
@@ -0,0 +1,13 @@
+class Usuario{
+    constructor(nombre, apellidoPaterno, apellidoMaterno, telefono, correo, pw, balance){
+
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.pw = pw;
+        this.balance = balance;
+    
+    }
+}
