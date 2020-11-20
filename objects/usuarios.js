40eaefd59fd5eadef089b51a88b754dcdfb27f30
Alfredo Paz Zamora <apaz0@ucol.mx>
2020-11-20 11:25:12 -0600

    Se agrego fechas de abonos y deduas

--- objects/usuarios.js
+++ objects/usuarios.js
@@ -1,5 +1,5 @@
 class Usuario{
-    constructor(nombre, apellidoPaterno, apellidoMaterno, telefono, correo, pw, balance){
+    constructor(nombre, apellidoPaterno, apellidoMaterno, telefono, correo, pw, balance, abono){
 
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
@@ -8,6 +8,7 @@ class Usuario{
         this.correo = correo;
         this.pw = pw;
         this.balance = balance;
-    
+        this.abono = [];
+        this.deuda = [];
     }
 }
